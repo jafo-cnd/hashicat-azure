d0587f35b96a575136c018cea1eee0785e7a89c7
jafo-cnd <114009016+jafo-cnd@users.noreply.github.com>
2023-02-16 15:14:28 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,7 @@
 module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
+  resource_group_name = "myresourcegroup"
+  use_for_each = "for_each"
   # insert the 2 required variables here
 }
